0d5de25b7fd5ece4f6407fc8883b7cb48ab3387b
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-09-20 11:33:01 -0600

    Guía I y II - Introducción a Dapper. Obtener todos los datos con Dapper. (finalizadas)

---
+++ Dapper Demo/Dapper Demo/Form1.cs
@@ -0,0 +1,34 @@
+﻿using AccesoDatos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dapper_Demo
+{
+    public partial class Form1 : Form
+    {
+        CustomerRepository customerR = new CustomerRepository();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtenerTodo_Click(object sender, EventArgs e)
+        {
+            var cliente = customerR.ObtenerTodo();
+            dgvObtenerTodo.DataSource = cliente;
+        }
+
+        private void btnObtenerPorId_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
